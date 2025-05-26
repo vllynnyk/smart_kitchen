244319116236c8eb1c5670826d425e658d97765f
vllynnyk <magnetto54@gmail.com>
2025-05-26 14:15:34 +0200

    Add test for ordering of DishType model by name

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,3 +8,12 @@ class DishTypeTest(TestCase):
     def test_dish_type_str(self):
         dish_type = DishType.objects.create(name="Soups")
         self.assertEqual(str(dish_type), dish_type.name)
+
+    def test_dish_type_ordering(self):
+        """
+        Test that DishType objects are ordered alphabetically by their 'name' field.
+        """
+        DishType.objects.create(name="Soups")
+        DishType.objects.create(name="Pizza")
+        all_dish_types = list(DishType.objects.all())
+        self.assertEqual(all_dish_types[0]. name, "Pizza")
