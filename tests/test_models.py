82ffdbaf09e63d39a500591ba19fcf9daf9b6ec9
vllynnyk <magnetto54@gmail.com>
2025-05-26 14:14:33 +0200

    Add test for __str__ method of DishType model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from kitchen_board.models import DishType
+
+
+class DishTypeTest(TestCase):
+
+    def test_dish_type_str(self):
+        dish_type = DishType.objects.create(name="Soups")
+        self.assertEqual(str(dish_type), dish_type.name)
