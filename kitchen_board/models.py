0c9332167c019b4248ac5b3d7749165dcaeee1ef
vllynnyk <magnetto54@gmail.com>
2025-05-26 14:12:33 +0200

    Create DishType model

--- kitchen_board/models.py
+++ kitchen_board/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=65, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
