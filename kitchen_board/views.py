cf53ef5f70e4db58142f0eda577af8ccb01cf622
vllynnyk <magnetto54@gmail.com>
2025-05-26 19:53:57 +0200

    Add index view funktion in kitchen_board/views.py

--- kitchen_board/views.py
+++ kitchen_board/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen_board.models import Cook, DishType, Dish, Ingredient
+
+
+@login_required
+def index(request):
+    """View function for home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+    num_ingredients = Ingredient.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+        "num_dish": num_dishes,
+        "num_ingredients": num_ingredients,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "kitchen_board/index.html", context=context)
