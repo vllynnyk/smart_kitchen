affa0e3bd8581a48409a9276527c70078a3579fd
vllynnyk <magnetto54@gmail.com>
2025-05-30 16:17:34 +0200

    Add SearchForm to filter Cooks by username

--- kitchen_board/forms.py
+++ kitchen_board/forms.py
@@ -40,3 +40,13 @@ class DishForm(forms.ModelForm):
     class Meta:
         model = Dish
         fields = "__all__"
+
+
+# Search Forms for Model Cook, DishType, Ingredient, Dish
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=100,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by Username"}),
+    )
