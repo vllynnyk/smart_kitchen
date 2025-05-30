8fecaf8af2eb47ea8b2dc15ff0e5b4617ff2c832
vllynnyk <magnetto54@gmail.com>
2025-05-30 17:42:34 +0200

    Override get_context_data and get_queryset to Ingredient search

--- kitchen_board/views.py
+++ kitchen_board/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from kitchen_board.forms import DishForm, CookCreationForm, CookPositionUpdateForm, DishTypeSearchForm
+from kitchen_board.forms import DishForm, CookCreationForm, CookPositionUpdateForm, DishTypeSearchForm, \
+    IngredientSearchForm
 from kitchen_board.models import Cook, DishType, Dish, Ingredient
 
 
@@ -79,6 +80,19 @@ class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
 class IngredientListView(LoginRequiredMixin, generic.ListView):
     model = Ingredient
     paginate_by = 10
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IngredientListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        context["search_form"] = IngredientSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        queryset = Ingredient.objects.all()
+        form = IngredientSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
 
 
 class IngredientCreateView(LoginRequiredMixin, generic.CreateView):
