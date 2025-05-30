8c2e781fa7f6c64ff4a9d4d41df3e0aa416ba1a3
vllynnyk <magnetto54@gmail.com>
2025-05-30 08:54:57 +0200

    Add URL pattern for DishTypeListView

--- kitchen_board/urls.py
+++ kitchen_board/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from kitchen_board.views import index
+from kitchen_board.views import index, DishTypeListView
 
 
 app_name = "kitchen_board"
 
 urlpatterns = [
     path("", index, name="index"),
+    #DishType URLs
+    path(
+        "dish_types/",
+         DishTypeListView.as_view(),
+         name="dish_type_list"
+    ),
 ]
