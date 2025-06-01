8b941b94daab89cb03666820a221d66fdb903347
vllynnyk <magnetto54@gmail.com>
2025-06-01 15:19:04 +0200

    Add PrivateDishTypeTest with setup

--- tests/test_views_dish_type.py
+++ tests/test_views_dish_type.py
@@ -1,11 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from kitchen_board.models import DishType
 
 DISH_TYPES_URL = reverse("kitchen_board:dish_type_list")
 
+
 class PublicDishTypeTest(TestCase):
-    def test_login_required(self):
+    def test_login_required(self) -> None:
         """Test that login is required for views."""
         response = self.client.get(DISH_TYPES_URL)
         self.assertNotEqual(response.status_code, 200)
+
+
+class PrivateDishTypeTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="user1",
+            password="1234pass",
+        )
+        DishType.objects.create(name="Pizza")
+        DishType.objects.create(name="Soups")
+        DishType.objects.create(name="Salads")
+
+    def setUp(self) -> None:
+        self.client.force_login(self.user)
