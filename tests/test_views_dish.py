68c5fb60cf59fad7124b8c4934244ad97f304bcc
vllynnyk <magnetto54@gmail.com>
2025-06-01 19:15:28 +0200

    Add PrivateDishTest with setup

--- tests/test_views_dish.py
+++ tests/test_views_dish.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-
+from kitchen_board.models import Dish
 
 DISH_URL = reverse("kitchen_board:dish_list")
 
@@ -12,3 +13,18 @@ class PublicDishTest(TestCase):
         response = self.client.get(DISH_URL)
         self.assertNotEqual(response.status_code, 200)
 
+
+class PrivateDishTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="user1",
+            password="1234pass",
+        )
+        Dish.objects.create(name="Margarita")
+        Dish.objects.create(name="Curry")
+        Dish.objects.create(name="Sushi")
+
+    def setUp(self) -> None:
+        self.client.force_login(self.user)
