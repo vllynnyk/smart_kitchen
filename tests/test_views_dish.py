3a18247f76ad14c626e470e909af35c0ee79ffec
vllynnyk <magnetto54@gmail.com>
2025-06-02 20:36:19 +0200

    Add CRUD tests for Dish view

--- tests/test_views_dish.py
+++ tests/test_views_dish.py
@@ -70,3 +70,48 @@ class PrivateDishTest(TestCase):
         self.assertContains(response,
                             "There are no dishes on the Kitchen Board.",
                             html=True)
+
+    def test_create_dish(self):
+        response = self.client.post(reverse("kitchen_board:dish_create"),{"name": "Meatballs"})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response,DISH_URL)
+        exists = Dish.objects.filter(name="Meatballs").exists()
+        self.assertTrue(exists)
+
+    def test_update_dish(self):
+        dish = Dish.objects.get(name="Curry")
+        response = self.client.post(reverse(
+            "kitchen_board:dish_update",
+            kwargs={"pk": dish.pk}
+        ),
+            {"name": "Meatballs"})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, DISH_URL)
+        exists = Dish.objects.filter(name="Meatballs").exists()
+        self.assertTrue(exists)
+
+    def test_delete_dish(self):
+        dish = Dish.objects.get(name="Sushi")
+        response = self.client.post(
+            reverse(
+                "kitchen_board:dish_delete",
+                kwargs={"pk": dish.pk}
+            )
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, DISH_URL)
+        exists = Dish.objects.filter(name="Sushi").exists()
+        self.assertFalse(exists)
+
+    def test_detail_dish(self):
+        dish = Dish.objects.get(name="Margarita")
+        response = self.client.get(
+            reverse(
+                "kitchen_board:dish_detail",
+                kwargs={"pk": dish.pk}
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,dish.name)
+        self.assertTemplateUsed(response, "kitchen_board/dish_detail.html")
+        self.assertEqual(response.context["dish"], dish)
