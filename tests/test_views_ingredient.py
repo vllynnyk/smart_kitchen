2e469567b84da436fda73962a39bc1d3967da51b
vllynnyk <magnetto54@gmail.com>
2025-06-02 20:52:31 +0200

    Add PrivateIngredientTest with setup

--- tests/test_views_ingredient.py
+++ tests/test_views_ingredient.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from kitchen_board.models import Ingredient
 
 INGREDIENT_URL = reverse("kitchen_board:ingredient_list")
 
@@ -11,3 +13,18 @@ class PublicIngredientTest(TestCase):
         response = self.client.get(INGREDIENT_URL)
         self.assertNotEqual(response.status_code, 200)
 
+
+class PrivateIngredientTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="user1",
+            password="1234pass",
+        )
+        Ingredient.objects.create(name="Apple")
+        Ingredient.objects.create(name="Tomato")
+        Ingredient.objects.create(name="Cheese")
+
+    def setUp(self) -> None:
+        self.client.force_login(self.user)
