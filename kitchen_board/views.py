be13631ccdbc74a670293ab3065ef0ac006cc966
vllynnyk <magnetto54@gmail.com>
2025-06-09 10:04:31 +0200

    Remove visits count from index

--- kitchen_board/views.py
+++ kitchen_board/views.py
@@ -26,15 +26,11 @@ def index(request):
     num_dishes = Dish.objects.count()
     num_ingredients = Ingredient.objects.count()
 
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-
     context = {
         "num_cooks": num_cooks,
         "num_dish_types": num_dish_types,
         "num_dishes": num_dishes,
         "num_ingredients": num_ingredients,
-        "num_visits": num_visits + 1,
     }
 
     return render(request, "kitchen_board/index.html", context=context)
